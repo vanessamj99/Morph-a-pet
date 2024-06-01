8bea734786ef9983a2ab0fff9e45e9dcf0710e95
Vanessa Johnson <vmjohnson999@gmail.com>
2024-06-01 09:21:26 -0400

    Initial Commit

---
+++ VirtualPet Watch App/VirtualPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualPetApp.swift
+//  VirtualPet Watch App
+//
+//  Created by Vanessa Johnson on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct VirtualPet_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VirtualPet/VirtualPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualPetApp.swift
+//  VirtualPet
+//
+//  Created by Vanessa Johnson on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct VirtualPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
